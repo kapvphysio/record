5409ab67cb4dac999e6e8809be047705ea862763
gnanasenthil654321 <gnanasenthil654321@gmail.com>
2018-08-02 16:44:29 +0530

    complete shell script, completed first page and certificate and new file finalRecord.pdf shall be created

--- completeDocCreator.sh
+++ completeDocCreator.sh
@@ -1,3 +1,4 @@
 latexmk -pdf
 awk -f forToc.awk combinedRecord.toc > customTOC.tex
 latexmk -pdf customTOC.tex
+pdftk ./firstPageAndCertificate.pdf ./customTOC.pdf ./combinedRecord.pdf cat output finalRecord.pdf
