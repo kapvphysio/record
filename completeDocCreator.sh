b6c2ec5635c08e6d7713d58bd501b8cf1202e220
kapvphysio <kapvphysio@gmail.com>
2018-08-14 18:09:33 +0530

    proof reading 2

--- completeDocCreator.sh
+++ completeDocCreator.sh
@@ -1,5 +1,6 @@
 latexmk -pdf combinedRecord.tex
+pdftk ./combinedRecord.pdf cat 3-end output modifiedCombinedRecord.pdf
 awk -f forToc.awk combinedRecord.toc > customTOC.tex
 latexmk -pdf firstPageAndCertificate.tex
 latexmk -pdf customTOC.tex
-pdftk ./firstPageAndCertificate.pdf ./customTOC.pdf ./combinedRecord.pdf cat output finalRecord.pdf
+pdftk ./firstPageAndCertificate.pdf ./customTOC.pdf ./modifiedCombinedRecord.pdf cat output finalRecord.pdf
